d470a13f7663f5ae1abe5acf8eb010b1e8e0b5bb
slagme <slawomir.gmerek@gmail.com>
2017-06-01 21:06:15 +0200

    Validation added to Task Entity

--- src/MainBundle/Entity/Task.php
+++ src/MainBundle/Entity/Task.php
@@ -3,6 +3,7 @@
 namespace MainBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Task
@@ -23,7 +24,9 @@ class Task
 
     /**
      * @var string
-     *
+     * @Assert\NotBlank(
+     *  message="Name cannot be empty."
+     * ) 
      * @ORM\Column(name="Name", type="string", length=60)
      */
     private $name;
@@ -36,13 +39,28 @@ class Task
     private $description;
 
     /**
-     * @var string
-     *
-     * @ORM\Column(name="Time", type="string", length=255)
+     * @var integer
+     * @Assert\Range(
+     *  min=0, 
+     *  minMessage="Minutes value must be at least {{ limit }}"
+     * )
+     * @ORM\Column(name="Minutes", type="integer", length=3)
      */
-    private $time;
+    private $minutes;
 
-
+    /**
+     
+     * @var type* @var integer
+     * @Assert\Range(
+     *  min=0,
+     *  max=60, 
+     *  minMessage="Seconds value must be at least {{ limit }} seconds",
+     *  maxMessage="Seconds value cannot be bigger than {{ limit }} seconds"
+     * )
+     * @ORM\Column(name="Seconds", type="integer", length=2)
+     */
+    
+    private $seconds;
     /**
      * Get id
      *
@@ -120,5 +138,51 @@ class Task
     public function getTime()
     {
         return $this->time;
+    }
+
+    /**
+     * Set minutes
+     *
+     * @param integer $minutes
+     * @return Task
+     */
+    public function setMinutes($minutes)
+    {
+        $this->minutes = $minutes;
+
+        return $this;
+    }
+
+    /**
+     * Get minutes
+     *
+     * @return integer 
+     */
+    public function getMinutes()
+    {
+        return $this->minutes;
+    }
+
+    /**
+     * Set seconds
+     *
+     * @param integer $seconds
+     * @return Task
+     */
+    public function setSeconds($seconds)
+    {
+        $this->seconds = $seconds;
+
+        return $this;
+    }
+
+    /**
+     * Get seconds
+     *
+     * @return integer 
+     */
+    public function getSeconds()
+    {
+        return $this->seconds;
     }
 }
